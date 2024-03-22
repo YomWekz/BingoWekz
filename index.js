1f899a45ca1ebee6819cfea8caa0f288ad808ab1
Lizerwekz Medina <158233294+YomWekz@users.noreply.github.com>
2024-03-22 09:58:49 +0800

    index.js
    UPDATED .js

--- index.js
+++ index.js
@@ -1,94 +1,247 @@
+console.log("Welcome to the game")
 let B = false,
-    I = false,
-    N = false,
-    G = false,
-    O = false;
+  I = false,
+  N = false,
+  G = false,
+  O = false;
+let a = [];
+
+function newNumber(maxNumber) {
+  let x = Math.floor(Math.random() * maxNumber) + 1;
+  let x1 = Math.floor(Math.random() * 15) + 1;
+  let x2 = Math.floor(Math.random() * 15) + 1;
+  let x3 = Math.floor(Math.random() * 15) + 1;
+  let x4 = Math.floor(Math.random() * 15) + 1;
+
+
+  let i1 = Math.floor(Math.random() * 15) + 16;
+  let i2 = Math.floor(Math.random() * 15) + 16;
+  let i3 = Math.floor(Math.random() * 15) + 16;
+  let i4 = Math.floor(Math.random() * 15) + 16;
+  let i5 = Math.floor(Math.random() * 15) + 16;
+
+
+  let n1 = Math.floor(Math.random() * 15) + 31;
+  let n2 = Math.floor(Math.random() * 15) + 31;
+  let n3 = Math.floor(Math.random() * 15) + 31;
+  let n4 = Math.floor(Math.random() * 15) + 31;
+  let n5 = Math.floor(Math.random() * 15) + 31;
+
+
+  let g1 = Math.floor(Math.random() * 15) + 46;
+  let g2 = Math.floor(Math.random() * 15) + 46;
+  let g3 = Math.floor(Math.random() * 15) + 46;
+  let g4 = Math.floor(Math.random() * 15) + 46;
+  let g5 = Math.floor(Math.random() * 15) + 46;
+
+
+  let o1 = Math.floor(Math.random() * 15) + 60;
+  let o2 = Math.floor(Math.random() * 15) + 60;
+  let o3 = Math.floor(Math.random() * 15) + 60;
+  let o4 = Math.floor(Math.random() * 15) + 60;
+  let o5 = Math.floor(Math.random() * 15) + 60;
+
+  //if ... else if...else statement
+  //conditional ope
+  // GTE > =, LTE < =,EQ ==,NEQ ! =, AND &&, OR ||
+  if (x <= 15) {
+    console.log(`New number ${x} belongs to "B" `);
+    B = true;
+    BNum = x;
+    if (!a.includes(x)) {
+
+      document.querySelector("#td1").textContent = x;
+      a.push(x);
+
+    }
+    if (!a.includes(x1)) {
+
+      document.querySelector("#td6").textContent = x1;
+      a.push(x1);
+
+    }
+    if (!a.includes(x2)) {
+
+      document.querySelector("#td11").textContent = x2;
+      a.push(x2);
+
+    } if (!a.includes(x3)) {
+
+      document.querySelector("#td16").textContent = x3;
+      a.push(x3);
+
+    } if (!a.includes(x4)) {
+
+      document.querySelector("#td21").textContent = x4;
+      a.push(x4);
+
+    }
+
+
+  } else if (x >= 16 && x <= 30) {
+    console.log(`New number ${x} belongs to "I" `);
+    I = true;
+    INum = x;
+
+   if (!a.includes(i1)) {
+
+      document.querySelector("#td2").textContent = i1;
+      a.push(i1);
+
+    }
+    if (!a.includes(i2)) {
+
+      document.querySelector("#td7").textContent = i2;
+      a.push(i2);
+
+    }
+    if (!a.includes(i3)) {
+
+      document.querySelector("#td12").textContent = i3;
+      a.push(i3);
+
+    } if (!a.includes(i4)) {
+
+      document.querySelector("#td17").textContent = i4;
+      a.push(i4);
+
+    } if (!a.includes(i5)) {
+
+      document.querySelector("#td22").textContent = i5;
+      a.push(i5);
+
+    }
+
+  } else if (x >= 30 && x <= 45) {
+    console.log(`New number ${x} belongs to "N" `);
+    N = true;
+    NNum = x;
+    if (!a.includes(n1)) {
+
+      document.querySelector("#td3").textContent = n1;
+      a.push(n1);
+
+    }
+    if (!a.includes(n2)) {
+
+      document.querySelector("#td8").textContent = n2;
+      a.push(n2);
+
+    }
+   
+     if (!a.includes(n4)) {
+
+      document.querySelector("#td18").textContent = n4;
+      a.push(n4);
+
+    } if (!a.includes(n5)) {
+
+      document.querySelector("#td23").textContent = n5;
+      a.push(n5);
+
+    }
+  } else if (x >= 45 && x <= 60) {
+    console.log(`New number ${x} belongs to "G" `);
+    G = true;
+    GNum = x;
+    if (!a.includes(g1)) {
+
+      document.querySelector("#td4").textContent = g1;
+      a.push(g1);
+
+    }
+    if (!a.includes(g2)) {
+
+      document.querySelector("#td9").textContent = g2;
+      a.push(g2);
+
+    }
+    if (!a.includes(g3)) {
+
+      document.querySelector("#td14").textContent = g3;
+      a.push(g3);
+
+    } if (!a.includes(g4)) {
+
+      document.querySelector("#td19").textContent = g4;
+      a.push(g4);
+
+    } if (!a.includes(g5)) {
+
+      document.querySelector("#td24").textContent = g5;
+      a.push(g5);
+
+    }
     
-    let BNumber, INumber, NNumber, GNumber, ONumber;
+  } else if (x >= 60 && x <= 75) {
+    console.log(`New number ${x} belongs to "O" `);
+    O = true;
+    ONum = x;
+    if (!a.includes(o1)) {
 
-function newNumber() {
-    let x = Math.floor(Math.random() * 75) + 1;
-    if (x <= 15) {
-        generateNewNumber(15);
-        B = true;
-        BNumber = x;
-    } else if (x >= 16 && x <= 30) {
-        generateNewNumber(30);
-        I = true;
-        INumber = x;
-    }
-    else if (x >= 31 && x <= 42) {
-        generateNewNumber(42);
-        N = true;
-        NNumber = x;
-    }
-    else if (x >= 46 && x <= 60) {
-        generateNewNumber(60);
-        G = true;
-        GNumber = x;
-    }
-    else if (x >= 61 && x <= 75) {
-        generateNewNumber(75);
-        O = true;
-        ONumber = x;
-    }
+      document.querySelector("#td5").textContent = o1;
+      a.push(o1);
 
-    if (B == true && I == true && N == true && G == true && O == true) {
-        console.log("BINGOOOOO BES PRAMISESS")
-        console.table({
-            B: BNumber,
-            I: INumber,
-            N: NNumber,
-            G: GNumber,
-            O: ONumber,
-        })
     }
+    if (!a.includes(o2)) {
+
+      document.querySelector("#td10").textContent = o2;
+      a.push(o2);
+
+    }
+    if (!a.includes(o3)) {
+
+      document.querySelector("#td15").textContent = o3;
+      a.push(o3);
+
+    } if (!a.includes(o4)) {
+
+      document.querySelector("#td20").textContent = o4;
+      a.push(o4);
+
+    } if (!a.includes(o5)) {
+
+      document.querySelector("#td25").textContent = o5;
+      a.push(o5);
+
+    }
+   
+  }
+
+  else {
+    console.log(`New number ${x} is invalid.`);
+  }
+  if (B == true && I == true && N == true && G == true && O == true) {
+    console.log(`Pre naka Bingo ka Wow!!!!`);
+    console.table({
+      B: BNum,
+      I: INum,
+      N: NNum,
+      G: GNum,
+      O: ONum,
+    });
+  }
+
 
 }
+newNumber(75);
 
-function generateNewNumber(maxNumber) {
-    const min = (maxNumber - 15) + 1;
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(maxNumber);
+// let counter = 5;
 
-    let x = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+// for (counter; counter > 0; counter--) {
+// console.log(`Current Counter is ${counter}`);
+//if (counter == 2) break;
+// }
 
-    switch (maxNumber) {
-        case 15:
-            return console.log(`B: ${x}`)
-        case 30:
-            return console.log(`I: ${x}`)
-        case 42:
-            return console.log(`N: ${x}`)
-        case 60:
-            return console.log(`G: ${x}`)
-        case 75:
-            return console.log(`O: ${x}`)
-    }
+// while (counter > 0)
+// {
+//  console.log(`While, Current counter is ${counter}`);
+// if (counter == 2) break;
 
-}
-console.log(newNumber());
+// newNumber(75);
+// counter-- ;
+// }
 
-//LOOPS
-//let counter = 10
-//for( counter; counter >= 0; counter--) {
- // console.log(current counter is ${counter});
-////if (counter ==3) break;
-////newNumber(75)
-//}
-
-//let counter = 10
-//while(counter >= 0) {
-//console.log(current counter is ${counter});
-///counter--;
-//}
-
-//counter = 5
-//do{
-  //  console.log(current counter is ${counter});
- //   counter--;
-
-//}while(counter >= 0);
-while(!B||!I||!N||!G||!O){
+while (B == false || I == false || N == false || G == false || O == false) {
   newNumber(75);
 }
